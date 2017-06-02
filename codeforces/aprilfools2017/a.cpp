a5d791fa2c632e3c3f3cf4385745ee6edc76720d
Sotaro Sugimoto <sotaro.sugimoto@gmail.com>
2017-06-01 18:21:40 -0700

    April Fools Contest 2017 a

---
+++ codeforces/aprilfools2017/a.cpp
@@ -0,0 +1,44 @@
+// MISCELLANEOUS
+#include <iostream>//cin/cout/wcin/wcout/left/right/internal/dec/hex/oct/fixed/scientific
+
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    switch(n) {
+        case 1: { n = 4; break; }
+        case 2: { n = 22; break; }
+        case 3: { n = 27; break; }
+        case 4: { n = 58; break; }
+        case 5: { n = 85; break; }
+        case 6: { n = 94; break; }
+        case 7: { n = 121; break; }
+        case 8: { n = 166; break; }
+        case 9: { n = 202; break; }
+        case 10: { n = 265; break; }
+        case 11: { n = 274; break; }
+        case 12: { n = 319; break; }
+        case 13: { n = 346; break; }
+        case 14: { n = 355; break; }
+        case 15: { n = 378; break; }
+        case 16: { n = 382; break; }
+        case 17: { n = 391; break; }
+        case 18: { n = 438; break; }
+        case 19: { n = 454; break; }
+        case 20: { n = 483; break; }
+        case 21: { n = 517; break; }
+        case 22: { n = 526; break; }
+        case 23: { n = 535; break; }
+        case 24: { n = 562; break; }
+        case 25: { n = 576; break; }
+        case 26: { n = 588; break; }
+        case 27: { n = 627; break; }
+        case 28: { n = 634; break; }
+        case 29: { n = 636; break; }
+        case 30: { n = 645; break; }
+    }
+    cout << n << '\n';
+    return 0;
+}
