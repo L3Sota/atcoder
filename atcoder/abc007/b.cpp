4ced3e7fe5b5420feeea0cea932dfa96c6fc364e
Sotaro Sugimoto <sotaro.sugimoto@gmail.com>
2018-01-18 03:26:45 +0900

    ABC007 b

---
+++ atcoder/abc007/b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>//cin/cout/wcin/wcout/left/right/internal/dec/hex/oct/fixed/scientific
+#include <string>//stoi/stol/stoul/stoll/stoull/stof/stod/stold/to_string/getline
+
+using namespace std;
+
+int main(void) {
+    string s;
+    cin >> s;
+
+    cout << (s == "a" ? "-1" : "a") << endl;
+    return 0;
+}
